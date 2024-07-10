f340e94599c7545ccd43ec5affe8f796c94b74a6
JosueEC <josueev.cruz@gmail.com>
2024-07-10 17:55:23 -0600

    [FEAT](auth): Add the login, register and verify logic methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,128 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import * as bcrypt from 'bcrypt';
+import { envs } from 'src/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger();
+
+  constructor(private readonly jwtService: JwtService) {
+    super();
+  }
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('MongoDB connected');
+  }
+
+  async signJWT(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
+  }
+
+  async verifyToken(token: string) {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { sub, iat, exp, ...user } = this.jwtService.verify(token, {
+        secret: envs.jwtSecret,
+      });
+
+      return {
+        user: user,
+        token: await this.signJWT(user),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 401,
+        message: 'Invalid token',
+      });
+    }
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    try {
+      const { email, name, password } = registerUserDto;
+
+      const user = await this.user.findFirst({
+        where: { email: email },
+      });
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exists',
+        });
+      }
+
+      const newUser = await this.user.create({
+        data: {
+          email: email,
+          password: bcrypt.hashSync(password, 10),
+          name: name,
+        },
+      });
+
+      /**
+       * El operador ...rest nos permite desestructurar las propiedad
+       * restantes de un objeto, de esta forma es como quitamos del
+       * objeto la propiedad password para que esta no sea devuelta al
+       * cliente
+       */
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: __, ...rest } = newUser;
+
+      return {
+        user: rest,
+        token: await this.signJWT(rest),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    try {
+      const { email, password } = loginUserDto;
+
+      const user = await this.user.findFirst({
+        where: { email: email },
+      });
+
+      if (!user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid',
+        });
+      }
+
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid',
+        });
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: __, ...rest } = user;
+
+      return {
+        user: rest,
+        token: await this.signJWT(rest),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+}
